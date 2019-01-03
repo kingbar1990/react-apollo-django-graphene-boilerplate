5d491d99a351110d2b7e41f4a482896876148d89
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-01-03 18:54:00 +0200

    Chart on task Stat element

--- client/src/components/Dashboard/Stats/index.js
+++ client/src/components/Dashboard/Stats/index.js
@@ -14,47 +14,44 @@ class Stat extends React.Component {
         }
     }
 
-    componentDidMount() {
-        this.setState({
-            dataPie: {
-                labels: ['In progress', 'Done', 'To Do'],
-                datasets: [
-                    {
-                        data: [this.props.tasks.tasks && this.props.tasks.tasks.filter(task => task.status === 0),
-                            this.props.tasks.tasks && this.props.tasks.tasks.filter(task => task.status === 1),
-                            this.props.tasks.tasks && this.props.tasks.tasks.filter(task => task.status === 2)],
-                        backgroundColor: [
-                            "#F7464A",
-                            "#46BFBD",
-                            "#FDB45C",
-                        ],
-                        hoverBackgroundColor: [
-                            "#FF5A5E",
-                            "#5AD3D1",
-                            "#FFC870",
-                        ]
-                    }
-                ]
-            }
-        })
-    }
-
-
     render() {
-        console.log(this.props)
+
+        if (this.props.tasks.loading) {
+            return <div className="loader"></div>
+        }
+        let {tasks} = this.props.tasks
+        let {users} = this.props.users
+
+        let dataPie = {
+            labels: ['In progress', 'Done', 'To Do'],
+            datasets: [
+                {
+                    data: [tasks.filter(task => task.status === 0).length,
+                        tasks.filter(task => task.status === 1).length,
+                        tasks.filter(task => task.status === 2).length],
+                    backgroundColor: [
+                        "#F7464A",
+                        "#46BFBD",
+                        "#FDB45C",
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF5A5E",
+                        "#5AD3D1",
+                        "#FFC870",
+                    ]
+                }
+            ]
+        }
+
         return (
             <MDBContainer>
                 <MDBCard className="card-body"
                          style={{width: "22rem", marginTop: "1rem"}}>
                     <MDBCardTitle>{this.state.type}</MDBCardTitle>
                     <MDBCardText>
-                        <Pie data={this.state.dataPie}
-                             options={{responsive: true}}/>
-                        {this.state.type === 'tasks' ? (
-                            this.props.tasks.tasks && this.props.tasks.tasks.length
-                        ) : (
-                            this.props.users.users && this.props.users.users.length
-                        )
+                        {this.state.type === 'tasks' ? (tasks.length) &&
+                            <Pie data={dataPie} options={{responsive: true}}/>
+                            : (users.length)
                         }
                     </MDBCardText>
                 </MDBCard>
@@ -65,5 +62,8 @@ class Stat extends React.Component {
 
 export default compose(
     graphql(getUsers, {name: 'users'}),
-    graphql(getTasks, {name: 'tasks'}),
+    graphql(getTasks, {
+        options: {fetchPolicy: 'no-cache'},
+        name: 'tasks',
+    }),
 )(Stat)
--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,22 +1,36 @@
 import React from "react";
-import ReactTable from 'react-table'
 import "react-table/react-table.css";
 
 import Dashboard from "../../containers/Dashboard";
 import Stat from "../../components/Dashboard/Stats";
+import {getTasks, getUsers} from "../../queries";
+import {compose, graphql} from "react-apollo/index";
 
 
 class Main extends React.Component {
-  render() {
-    return (
-      <Dashboard>
-        <React.Fragment>
-          <Stat type={'users'} />
-          <Stat type={'tasks'} />
-        </React.Fragment>
-      </Dashboard>
-    );
-  }
+    render() {
+        if (this.props.tasks.loading) {
+            return (<Dashboard>
+                    <div className="loader"></div>
+                </Dashboard>
+            )
+        }
+
+        return (
+            <Dashboard>
+                <React.Fragment>
+                    <Stat type={'users'} users={this.props.users}/>
+                    <Stat type={'tasks'} tasks={this.props.tasks}/>
+                </React.Fragment>
+            </Dashboard>
+        );
+    }
 }
 
-export default Main;
+export default compose(
+    graphql(getUsers, {name: 'users'}),
+    graphql(getTasks, {
+        options: {fetchPolicy: 'no-cache'},
+        name: 'tasks',
+    }),
+)(Main)
