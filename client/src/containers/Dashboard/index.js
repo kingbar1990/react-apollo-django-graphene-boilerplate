d30394f40265e5a4c45624a919a24231959beb94
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-01-09 19:02:24 +0200

    Relative footer

--- client/src/containers/Dashboard/index.js
+++ client/src/containers/Dashboard/index.js
@@ -4,18 +4,21 @@ import TopNavigation from "../../components/TopNavigation";
 import SideNavigation from "../../components/SideNavigation";
 import Footer from "../../components/Footer";
 
+import '../../index.css'
 
 class Dashboard extends Component {
   render() {
     const { children } = this.props;
     return (
       <div className="flexible-content">
-        <TopNavigation />
         <SideNavigation />
-        <main id="content" className="p-5">
-          {children}
-        </main>
-        <Footer />
+        <div className="main-container">
+        <TopNavigation />
+          <main id="content" className="p-5">
+            {children}
+          </main>
+          <Footer />
+        </div>
       </div>
     );
   }
