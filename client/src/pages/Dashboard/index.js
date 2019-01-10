a153fd6abd3744ecb2a018543f1fe2569a656a08
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-01-10 18:22:08 +0200

    Delete container at pages/dashboard/index

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { MDBContainer } from "mdbreact";
 
 import Dashboard from "../../containers/Dashboard";
 import GetTasks from "../../components/Statistics/GetTasks";
 import GetUsers from "../../components/Statistics/GetUsers";
 
-import "react-table/react-table.css";
 export default class Main extends React.Component {
   render() {
     return (
       <Dashboard>
-        <MDBContainer>
-          <GetUsers />
-          <GetTasks />
-        </MDBContainer>
+        <GetUsers />
+        <GetTasks />
       </Dashboard>
     );
   }
