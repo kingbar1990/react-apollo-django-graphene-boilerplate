a5a217355e370717c26f1f050b08b9344921b08a
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-01-18 12:46:22 +0200

    Fix side effect on private router

--- client/src/hocs/PrivateRoute.js
+++ client/src/hocs/PrivateRoute.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { graphql, compose } from 'react-apollo'
-import { withRouter } from 'react-router';
+import { graphql, compose } from 'react-apollo';
 import { verifyToken } from '../queries';
 
 export const withAuth = (ProtectedRoute) => {
     class PrivateRoute extends React.Component {
        async  componentWillMount() {
             try {
-                const token = JSON.parse(window.localStorage.getItem("token")).token
+                const token = JSON.parse(window.localStorage.getItem("token")).token;
                 const auth = await this.props.authorization({
                     variables: {
                         token: token
                     }
-                })
+                });
                 return auth;
             } catch (error) {
+                window.localStorage.removeItem("token");
                 return this.props.history.push("/login")
             }
         }
@@ -23,7 +23,6 @@ export const withAuth = (ProtectedRoute) => {
         }
     }
     return compose(
-        withRouter,
         graphql(verifyToken, {
             name: 'authorization',
         }),
