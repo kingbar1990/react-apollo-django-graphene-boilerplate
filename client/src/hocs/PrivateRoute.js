a141330ebfae55556832146f83d273b1d1838eb4
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-01-21 14:56:15 +0200

    Add logout

--- client/src/hocs/PrivateRoute.js
+++ client/src/hocs/PrivateRoute.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import { graphql, compose } from 'react-apollo';
-import { verifyToken } from '../queries';
+import React from "react";
+import { graphql, compose } from "react-apollo";
+import { connect } from "react-redux";
 
-export const withAuth = (ProtectedRoute) => {
-    class PrivateRoute extends React.Component {
-       async  componentWillMount() {
-            try {
-                const token = JSON.parse(window.localStorage.getItem("token")).token;
-                const auth = await this.props.authorization({
-                    variables: {
-                        token: token
-                    }
-                });
-                return auth;
-            } catch (error) {
-                window.localStorage.removeItem("token");
-                return this.props.history.push("/login")
-            }
-        }
-        render() {
-            return <ProtectedRoute {...this.props} /> 
-        }
-    }
-    return compose(
-        graphql(verifyToken, {
-            name: 'authorization',
-        }),
-    )(PrivateRoute)
-}
+import { logout } from "../actions";
+import { verifyToken } from "../queries";
+
+export const withAuth = ProtectedRoute => {
+   class PrivateRoute extends React.Component {
+      async componentWillMount() {
+         try {
+            const token = JSON.parse(window.localStorage.getItem("token")).token;
+            const auth = await this.props.authorization({
+               variables: {
+                  token: token
+               }
+            });
+            return auth;
+         } catch (error) {
+            this.props.isLogout();
+            return this.props.history.push("/login");
+         }
+      }
+      render() {
+         console.log(this.props.item);
+         return <ProtectedRoute {...this.props} />;
+      }
+   }
+   return compose(
+      graphql(verifyToken, {
+         name: "authorization"
+      }),
+      connect(
+         null,
+         {
+            isLogout: logout
+         }
+      )
+   )(PrivateRoute);
+};
