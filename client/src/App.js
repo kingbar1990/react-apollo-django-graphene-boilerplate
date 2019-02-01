0107b85a987643e356d46ed3623375e90ffb39bc
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-02-01 17:26:53 +0200

    Add a global loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import { withRouter, Switch, Route } from "react-router";
 
 import * as path from "./constants/routes";
@@ -6,12 +6,15 @@ import * as path from "./constants/routes";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Tasks from "./pages/Dashboard/Tasks";
-import Profile from "./pages/Dashboard/Profile";
+import Main from "./components/Main";
+import Loader from './components/Loader';
 import PageNotFound from "./components/PageNotFound";
-import ConfirmEmail from "./pages/ConfirmEmail";
-import ResetPassword from "./pages/ResetPassword";
+ 
+const Dashboard = lazy(() => import('./pages/Dashboard'))
+const Tasks = lazy(() => import('./pages/Dashboard/Tasks'));
+const Profile = lazy(() => import('./pages/Dashboard/Profile'));
+const ConfirmEmail = lazy(() => import('./pages/ConfirmEmail'));
+const ResetPassword = lazy(() => import('./pages/ResetPassword'));
 
 class App extends Component {
   render() {
@@ -20,12 +23,16 @@ class App extends Component {
         <Route exact path={path.HOME} component={Home} />
         <Route exact path={path.SIGN_UP} component={SignUp} />
         <Route exact path={path.SIGN_IN} component={Login} />
-        <Route exact path={path.DASHBOARD} component={Dashboard} />
-        <Route exact path={path.TASKS} component={Tasks} />
-        <Route exact path={path.PROFILE} component={Profile} />
-        <Route exact path={path.CONFIRM_EMAIL} component={ConfirmEmail} />
-        <Route exact path={path.RESET_PASSWORD} component={ResetPassword} />
-        <Route component={PageNotFound} />
+        <Main {...this.props}>
+            <Suspense fallback={<Loader />}>
+              <Route exact path={path.DASHBOARD} render={(props) => <Dashboard {...props} />} />
+              <Route exact path={path.TASKS} render={(props) => <Tasks {...props} />} />
+              <Route exact path={path.PROFILE} render={(props) => <Profile {...props} />} />
+              <Route exact path={path.CONFIRM_EMAIL} render={(props) => <ConfirmEmail {...props} />} />
+              <Route exact path={path.RESET_PASSWORD} render={(props) => <ResetPassword {...props} />} /> 
+            </Suspense>
+        </Main>
+        <Route exact component={PageNotFound} />
       </Switch>
     );
   }
