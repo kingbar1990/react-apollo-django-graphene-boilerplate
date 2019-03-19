cda9262c3d26e0ba877a9fffef849a6ef0081324
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-03-19 18:39:32 +0200

    Rework class 'NavBar' to functional component

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useCallback, Fragment } from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -7,131 +7,125 @@ import {
   MDBNavLink,
   MDBCollapse,
   MDBNavbarNav,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
   NavbarNav,
   Collapse,
   NavItem,
   Fa
 } from "mdbreact";
-import Popover from "react-simple-popover";
 
 import { DASHBOARD, PROFILE, TASKS } from "../../constants/routes";
-export default class NavBar extends Component {
-  state = {
-    open: false,
-    collapseID: ""
-  };
 
-  handleClick = () => {
-    this.setState({ open: !this.state.open });
-  };
+const NavBar = () => {
+  const [collapse, handleClick] = useState("");
+  const [width, setWidth] = useState(window.innerWidth);
 
-  handleClose = () => {
-    this.setState({ open: false });
-  };
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
 
-  toggleCollapse = collapseID => () => {
-    this.setState(state => {
-      if (state.collapseID !== collapseID) {
-        return { collapseID: collapseID };
-      }
-      return { collapseID: "" };
-    });
-  };
+  const toggleCollapse = useCallback(collapseID => () =>
+    handleClick((!collapse && collapseID) || "")
+  );
 
-  handleLogout = () => {
+  const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.href = "/login";
   };
 
-  render() {
-    return (
-      <Navbar className="flexible-navbar" light expand="md" scrolling>
-        <NavbarBrand href="/">Landing</NavbarBrand>
-        <MDBNavbarToggler onClick={this.toggleCollapse("navbarCollapse13")} />
-        <MDBCollapse id="navbarCollapse13" isOpen={this.state.collapseID} navbar>
-          <MDBNavbarNav left>
-            <MDBNavItem>
-              <MDBNavLink to={DASHBOARD}>Home</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to={PROFILE}>Profile</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to={TASKS}>Tasks</MDBNavLink>
-            </MDBNavItem>
-            <NavbarNav>
-              <NavItem>
-                <a
-                  rel="noopener noreferrer"
-                  className="nav-link Ripple-parent"
-                  href="https://www.rocklab.io"
-                  target="_blank"
-                >
-                  About <b>RockLab company</b>
-                </a>
-              </NavItem>
-            </NavbarNav>
-          </MDBNavbarNav>
-        </MDBCollapse>
-        <Collapse navbar>
-          <NavbarNav right>
+  return (
+    <Navbar className="flexible-navbar" light expand="md" scrolling>
+      <NavbarBrand href="/">Landing</NavbarBrand>
+      <MDBNavbarToggler onClick={toggleCollapse("navbarCollapse13")} />
+      <MDBCollapse id="navbarCollapse13" isOpen={collapse} navbar>
+        <MDBNavbarNav left>
+          <MDBNavItem>
+            <MDBNavLink to={DASHBOARD}>Home</MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to={PROFILE}>Profile</MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to={TASKS}>Tasks</MDBNavLink>
+          </MDBNavItem>
+          <NavbarNav>
             <NavItem>
               <a
-                className="nav-link navbar-link"
                 rel="noopener noreferrer"
-                target="_blank"
-                href="https://pl-pl.facebook.com/mdbootstrap/"
-              >
-                <Fa icon="facebook" />
-              </a>
-            </NavItem>
-            <NavItem>
-              <a
-                className="nav-link navbar-link"
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://twitter.com/mdbootstrap"
-              >
-                <Fa icon="twitter" />
-              </a>
-            </NavItem>
-            <NavItem>
-              <a
-                className="border border-light rounded mr-1 nav-link Ripple-parent"
-                rel="noopener noreferrer"
-                href="https://github.com/mdbootstrap/React-Bootstrap-with-Material-Design"
+                className="nav-link Ripple-parent"
+                href="https://www.rocklab.io"
                 target="_blank"
               >
-                <Fa icon="github" className="mr-2" />
-                RockLab GitHub
+                About <b>RockLab company</b>
               </a>
-            </NavItem>
-            <NavItem>
-              <div
-                style={{ display: "flex", cursor: "pointer" }}
-                className="nav-link navbar-link"
-                rel="noopener noreferrer"
-              >
-                <div className="button" ref="target" onClick={this.handleClick}>
-                  <Fa icon="user" className="mr-2" />
-                </div>
-                <Popover
-                  placement="bottom"
-                  container={this}
-                  target={this.refs.target}
-                  show={this.state.open}
-                  onHide={this.handleClose}
-                >
-                  <div className="popover-items">
-                    <MDBNavLink to={TASKS}>Tasks</MDBNavLink>
-                    <MDBNavLink to={PROFILE} onClick={this.handleLogout}>Log out</MDBNavLink>
-                  </div>
-                </Popover>
-              </div>
             </NavItem>
           </NavbarNav>
-        </Collapse>
-      </Navbar>
-    );
-  }
-}
+          {width <= 768 ? (
+            <Fragment>
+              <li className="nav-item">
+                <a className="nav-link" href="/login" onClick={handleLogout}>
+                  Logout
+                </a>
+              </li>
+            </Fragment>
+          ) : (
+            ""
+          )}
+        </MDBNavbarNav>
+      </MDBCollapse>
+      <Collapse navbar>
+        <NavbarNav right>
+          <NavItem>
+            <a
+              className="nav-link navbar-link"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://pl-pl.facebook.com/mdbootstrap/"
+            >
+              <Fa icon="facebook" />
+            </a>
+          </NavItem>
+          <NavItem>
+            <a
+              className="nav-link navbar-link"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://twitter.com/mdbootstrap"
+            >
+              <Fa icon="twitter" />
+            </a>
+          </NavItem>
+          <NavItem>
+            <a
+              className="border border-light rounded mr-1 nav-link Ripple-parent"
+              rel="noopener noreferrer"
+              href="https://github.com/mdbootstrap/React-Bootstrap-with-Material-Design"
+              target="_blank"
+            >
+              <Fa icon="github" className="mr-2" />
+              RockLab GitHub
+            </a>
+          </NavItem>
+          <MDBDropdown>
+            <MDBDropdownToggle nav>
+              <Fa icon="user" className="mr-2" />
+            </MDBDropdownToggle>
+            <MDBDropdownMenu className="dropdown-default" right>
+              <MDBNavLink to="#" onClick={handleLogout}>
+                Logout
+              </MDBNavLink>
+            </MDBDropdownMenu>
+          </MDBDropdown>
+        </NavbarNav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
