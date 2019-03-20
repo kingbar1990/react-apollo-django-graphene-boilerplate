4f7afca0310e33d7f91d30cf776967f5663b5eb1
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-03-20 17:21:48 +0200

    Remove plugin the react-simple-popove

--- client/src/components/DatePicker/index.js
+++ client/src/components/DatePicker/index.js
@@ -6,14 +6,12 @@ import { formatDate, parseDate } from "react-day-picker/moment";
 import "./style.css";
 
 const ExampleDatePicker = ({ onDateChange }) => (
-   <div>
-      <DayPickerInput
-         format="YYYY-MM-DD"
-         parseDate={parseDate}
-         placeholder={`${formatDate(new Date())}`}
-         onDayChange={onDateChange}
-      />
-   </div>
+  <DayPickerInput
+    format="YYYY-MM-DD"
+    parseDate={parseDate}
+    placeholder={`${formatDate(new Date())}`}
+    onDayChange={onDateChange}
+  />
 );
 
 export default ExampleDatePicker;
