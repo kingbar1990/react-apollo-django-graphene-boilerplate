7cec2403c74491303e72c9435a8b7b5e779ed4da
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2019-03-20 13:45:10 +0200

    Rework class 'GetTask' and  to functional component

--- client/src/components/Statistics/GetTasks.js
+++ client/src/components/Statistics/GetTasks.js
@@ -1,54 +1,55 @@
 import React from "react";
-import { MDBCard, MDBCardText } from "mdbreact";
+
 import { graphql, Query } from "react-apollo";
+import { MDBCard, MDBCardText } from "mdbreact";
+import { Pie } from "react-chartjs-2";
 
 import { getTasks } from "../../queries";
 
-import { Pie } from "react-chartjs-2";
-class GetTasks extends React.Component {
-  render() {
-    const stylesOnCard = { width: "22rem", marginTop: "1rem" };
-    return (
-      <Query query={getTasks} variables={{ page: 1 }}>
-        {({ loading, error, data }) => {
-          if (loading) return null
-          if (error) return `Error! ${error.message}`
-          const tasks = data.tasks.objects;
-          if (tasks.length) {
-            return (
-              <MDBCard className="card-body" style={stylesOnCard}>
-                <h3>Tasks: {tasks.length}</h3>
-                <MDBCardText>
-                  <Pie
-                    data={{
-                      labels: ["In progress", "Done", "To Do"],
-                      datasets: [
-                        {
-                          data: [
-                            tasks.filter(task => task.status === 0).length,
-                            tasks.filter(task => task.status === 1).length,
-                            tasks.filter(task => task.status === 2).length
-                          ],
-                          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
-                          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
-                        }
-                      ]
-                    }}
-                    options={{ responsive: true }}
-                  />
-                </MDBCardText>
-              </MDBCard>
-            )
-          }
+const GetTasks = () => {
+  const stylesOnCard = { width: "22rem", marginTop: "1rem" };
+  return (
+    <Query query={getTasks} variables={{ page: 1 }}>
+      {({ loading, error, data }) => {
+        if (loading) return null;
+        if (error) return `Error! ${error.message}`;
+        const tasks = data.tasks.objects;
+        if (tasks.length) {
           return (
             <MDBCard className="card-body" style={stylesOnCard}>
               <h3>Tasks: {tasks.length}</h3>
+              <MDBCardText>
+                <Pie
+                  data={{
+                    labels: ["In progress", "Done", "To Do"],
+                    datasets: [
+                      {
+                        data: [
+                          tasks.filter(task => task.status === 0).length,
+                          tasks.filter(task => task.status === 1).length,
+                          tasks.filter(task => task.status === 2).length
+                        ],
+                        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
+                        hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
+                      }
+                    ]
+                  }}
+                  options={{ responsive: true }}
+                />
+              </MDBCardText>
             </MDBCard>
-          )
-        }}
-      </Query>
-    );
-  }
-}
+          );
+        }
+        return (
+          <MDBCard className="card-body" style={stylesOnCard}>
+            <h3>Tasks: {tasks.length}</h3>
+          </MDBCard>
+        );
+      }}
+    </Query>
+  );
+};
 
-export default graphql(getTasks, {options: { fetchPolicy: "no-cache" }})(GetTasks);
+export default graphql(getTasks, { options: { fetchPolicy: "no-cache" } })(
+  GetTasks
+);
--- client/src/components/Statistics/GetUsers.js
+++ client/src/components/Statistics/GetUsers.js
@@ -5,23 +5,21 @@ import { MDBCard } from "mdbreact";
 
 import { getUsers } from "../../queries";
 
-class GetUsers extends React.Component {
-  render() {
-    const stylesOnCard = { width: "22rem", marginTop: "1rem" };
-    return (
-      <Query query={getUsers}>
-        {({ loading, error, data }) => {
-          if (loading) return null
-          if (error) return `Error! ${error.message}`
-          return (
-            <MDBCard className="card-body" style={stylesOnCard}>
-              <h3>Users: {data.users.length}</h3>
-            </MDBCard>
-          );
-        }}
-      </Query>
-    );
-  }
-}
+const GetUsers = () => {
+  const stylesOnCard = { width: "22rem", marginTop: "1rem" };
+  return (
+    <Query query={getUsers}>
+      {({ loading, error, data }) => {
+        if (loading) return null;
+        if (error) return `Error! ${error.message}`;
+        return (
+          <MDBCard className="card-body" style={stylesOnCard}>
+            <h3>Users: {data.users.length}</h3>
+          </MDBCard>
+        );
+      }}
+    </Query>
+  );
+};
 
-export default graphql(getUsers, {name: "users"})(GetUsers);
+export default graphql(getUsers, { name: "users" })(GetUsers);
